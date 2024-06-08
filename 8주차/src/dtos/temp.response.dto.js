73118804b50a24f66459b9e887623fc17e9a27c5
qwer97 <mcrohmh@naver.com>
2024-06-08 20:05:24 +0900

    8

---
+++ 8주차/src/dtos/temp.response.dto.js
@@ -0,0 +1,10 @@
+// temp.response.dto.js
+
+export const flagResponseDTO = (flag) => {
+    return {"flag" : flag};
+}
+
+
+export const tempResponseDTO = (data) => {
+    return {"testString" : data};
+}
