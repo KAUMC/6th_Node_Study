0d5aa6a0971c8366e85e757b15c36b2c3cf586f6
chadoli27 <jasminekim1009@gmail.com>
2024-06-12 01:32:47 +0900

    feat: 8주차

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// const express = require('express')   // common JS
-import express from 'express'          // ES6
+import express from 'express';
+import { tempRouter } from './src/routes/temp.route.js';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// router setting
+app.use('/temp', tempRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
